3fe2243a246270599e643eab6e370898f72fe5d5
ben <bjelinek42@gmail.com>
2022-09-12 10:54:06 -0600

    practice conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Joke from './joke'
 export default function App() {
   return (
     <div>
-      <Joke setUp='This is a setup 1.' punchLine='This is a punchline! 1' />
-      <Joke setUp='This is a setup 2.' punchLine='This is a punchline! 2' />
-      <Joke setUp='This is a setup 3.' punchLine='This is a punchline! 3' />
-      <Joke setUp='This is a setup 4.' punchLine='This is a punchline! 4' />
-      <Joke setUp='This is a setup 5.' punchLine='This is a punchline! 5' />
-      <Joke punchLine='This is a punchline! 6.' />
+      <Joke setUp='This is a setup 1.' punchLine='This is a punchline! 1' isPun={true} upvotes={10} comments={['Stop', 'Collaborate', 'And', 'Listen']} />
+      <Joke setUp='This is a setup 2.' punchLine='This is a punchline! 2' isPun={true} upvotes={12} />
+      <Joke setUp='This is a setup 3.' punchLine='This is a punchline! 3' isPun={false} upvotes={16} />
+      <Joke setUp='This is a setup 4.' punchLine='This is a punchline! 4' isPun={true} upvotes={34} />
+      <Joke setUp='This is a setup 5.' punchLine='This is a punchline! 5' isPun={false} upvotes={4} />
+      <Joke setUp={true} punchLine='This is a punchline! 6.' />
+      <Joke punchLine={true} />
     </div>
   )
 }
--- src/joke.js
+++ src/joke.js
@@ -7,6 +7,11 @@ export default function Joke(props) {
     <div>
       {props.setUp && <h1>Setup: {props.setUp}</h1>}
       <h2>Punchline: {props.punchLine}</h2>
+      {props.isPun && <h3>This joke is a pun!</h3>}
+      <h4>Upvotes = {props.upvotes}</h4>
+      {props.comments && props.comments.forEach(comment => {
+        <h4>{comment}</h4>
+      })}
     </div>
   )
 }
